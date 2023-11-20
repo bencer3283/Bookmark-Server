562fabc54f3592c2596c432042e86fbc239571e4
ChengPoSheng <bencer@outlook.com>
2023-11-20 18:01:32 +0200

    can reset now

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const fs = require('fs')
 
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+    let oldPage = JSON.parse(fs.readFileSync('current-page.json', {encoding: 'utf-8'}))
+    res.json(oldPage)
+})
+
 app.post('/', (req, res) => {
     console.log(req.body)
+    // read current page from file
     let oldPage = JSON.parse(fs.readFileSync('current-page.json', {encoding: 'utf-8'}))
     console.log(oldPage)
+    // if the recieved page number is greater than existing one, update it.
     let page = 0
     if (req.body.page > oldPage.page) {
         page = req.body.page
@@ -24,7 +31,23 @@ app.post('/', (req, res) => {
     fs.writeFile('current-page.json', JSON.stringify(newPage), function (err) {
         if (err) throw err;
         console.log('Saved!');
-    });
+    })
+    // send the updated page
+    res.json(newPage)
+})
+
+app.get('/reset', (req, res) => {
+    let resetTo = parseInt(req.query.pageNum)
+    console.log('reset to: ' + resetTo)
+    let oldPage = JSON.parse(fs.readFileSync('current-page.json', {encoding: 'utf-8'}))
+    let newPage = {
+        book: oldPage.book,
+        page: resetTo
+    }
+    fs.writeFile('current-page.json', JSON.stringify(newPage), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    })
     res.json(newPage)
 })
 
